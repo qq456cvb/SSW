1d5835448749c41a3adb0f407714bc95fab1b3bd
qq456cvb <447626601@qq.com>
2018-05-16 14:32:22 +0800

    Initial Commit

---
+++ SSW/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  SSW
+//
+//  Created by Neil on 16/05/2018.
+//  Copyright © 2018 Neil. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
